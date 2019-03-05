9aeb5973959b2ce0e44721b69e0d9383d0678c5e
RikiRC <47928112+RikiRC@users.noreply.github.com>
2019-03-05 14:42:11 +0100

    Update Disk usage 2 report.ps1

--- Reporting/Disk usage 2 report.ps1
+++ Reporting/Disk usage 2 report.ps1
@@ -6,21 +6,18 @@ $filename = $date + ".csv"
 
 foreach($line in Get-Content ip.txt)
 {
-	if($line -match $regex)
-	{	
-		$connectionResult = Test-Connection -ComputerName $line -Count 5 -Quiet
-		if ($connectionResult -eq $False)
-			{
-				"" >> $filename
-				$line + ",Connection status:,Failed" >> $filename
-			}
-			
-		elseif ($connectionResult -eq $True)
-			{
-				$disk = Get-Volume -DriveLetter $diskLetter
-				"" >> $filename
-				$line + ",Connection status:,OK" >> $filename
-				$disk | Format-List DriveLetter,OperationalStatus,@{N='Free Space [GB],';E={[math]::Round($_.SizeRemaining/1GB,2)}},@{N='Size [GB],';E={[math]::Round($_.Size/1GB,2)}} >> $filename
-			}
-	}
+	$connectionResult = Test-Connection -ComputerName $line -Count 5 -Quiet
+	if ($connectionResult -eq $False)
+		{
+			"" >> $filename
+			$line + ",Connection status:,Failed" >> $filename
+		}
+		
+	elseif ($connectionResult -eq $True)
+		{
+			$disk = Get-Volume -DriveLetter $diskLetter
+			"" >> $filename
+			$line + ",Connection status:,OK" >> $filename
+			$disk | Format-List DriveLetter,OperationalStatus,@{N='Free Space [GB],';E={[math]::Round($_.SizeRemaining/1GB,2)}},@{N='Size [GB],';E={[math]::Round($_.Size/1GB,2)}} >> $filename
+		}
 }
