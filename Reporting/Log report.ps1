8b05766418d3c5b8e15bd33b0c7d10daa9fa3a4f
RikiRC <47928112+RikiRC@users.noreply.github.com>
2019-03-05 14:42:43 +0100

    Update Log report.ps1

--- Reporting/Log report.ps1
+++ Reporting/Log report.ps1
@@ -8,21 +8,18 @@ $filename = $date + ".csv"
 
 foreach($line in Get-Content ip.txt)
 {
-	if($line -match $regex)
-	{	
-		$connectionResult = Test-Connection -ComputerName $line -Count 5 -Quiet
-		if ($connectionResult -eq $False)
-			{
-				"" >> $filename
-				$line + ",Connection status:,Failed" >> $filename
-			}
-			
-		elseif ($connectionResult -eq $True)
-			{
-				$getEvent = Get-EventLog $lType -EntryType $eType -Newest $ammount
-				"" >> $filename
-				$line + ",Connection status:,OK" >> $filename
-				$getEvent.Message >> $filename
-			}
-	}
+	$connectionResult = Test-Connection -ComputerName $line -Count 5 -Quiet
+	if ($connectionResult -eq $False)
+		{
+			"" >> $filename
+			$line + ",Connection status:,Failed" >> $filename
+		}
+		
+	elseif ($connectionResult -eq $True)
+		{
+			$getEvent = Get-EventLog $lType -EntryType $eType -Newest $ammount
+			"" >> $filename
+			$line + ",Connection status:,OK" >> $filename
+			$getEvent.Message >> $filename
+		}
 }
