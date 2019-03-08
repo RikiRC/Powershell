e1fe72344750e65c762664e4acb8f4d940b1020b
RikiRC <47928112+RikiRC@users.noreply.github.com>
2019-03-08 11:41:40 +0100

    Update Use-cred.ps1

--- Credentials/Use-cred.ps1
+++ Credentials/Use-cred.ps1
@@ -2,5 +2,3 @@ $user = get-content cred_user.txt
 $pass = get-content cred_password.txt | convertto-securestring
 
 $credential = new-object -TypeName System.Management.Automation.PSCredential -ArgumentList $user,$pass
-
-echo $user, $pass, $credential
